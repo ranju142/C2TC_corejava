dffbdd431524ccc598e32c463c79686fc33d3c8f
DELL <DELL@Ranjitha>
2022-10-16 19:33:51 +0530

    ternaryoperator done

---
+++ firstproject/src/firstproject/TernaryOperator.java
@@ -0,0 +1,13 @@
+package firstproject;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int v=1;
+		System.out.println(v==1 ?"Hello":"world");
+		v++;
+		System.out.println(v==1 ?"Hello":"world");
+
+	}
+
+}

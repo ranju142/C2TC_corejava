9d5a1e168e64264c1e4092d1c5ae152da872257e
DELL <DELL@Ranjitha>
2022-10-16 19:23:37 +0530

    ifexample done

---
+++ firstproject/src/firstproject/IfExample.java
@@ -0,0 +1,18 @@
+package firstproject;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+  int a=2;
+  if(a==1)  {
+	  System.out.println("one");
+  }
+  else if(a==2) {
+	  System.out.println("two");
+  }
+  else {
+	  System.out.println("invalid");
+  }
+	}
+
+}

9ef329878c10844252a3c6075cb989519760dfa9
H.Ranjitha <rajithah08@gmail.com>
2022-11-07 15:59:17 +0530

    if-else statement is done

---
+++ ControlStatement/src/Numerics1.java
@@ -0,0 +1,16 @@
+
+public class Numerics1 {
+
+	public static void main(String[] args) {
+		int X = 10;
+		int Y = 12;
+		if(X + Y < 10) {
+			System.out.println("X + Y is less than 10");
+	}else {
+		System.out.println("X + Y is greater than 20");
+		    
+
+	}
+
+}
+}

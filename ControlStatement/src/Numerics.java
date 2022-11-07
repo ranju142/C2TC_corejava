f486fd01b57996f74b470d22a1ff8769e0e8108a
H.Ranjitha <rajithah08@gmail.com>
2022-11-07 15:53:06 +0530

    Numerics Done

---
+++ ControlStatement/src/Numerics.java
@@ -0,0 +1,13 @@
+
+public class Numerics {
+
+	public static void main(String[] args) {
+		int X = 10;
+		int Y = 12;
+		if(X+Y > 20) {
+			System.out.println("X + Y is greater than 20");
+		}
+
+	}
+
+}

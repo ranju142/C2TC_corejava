ffb4246d27b4ff5c37840704ab3aad906841bbcd
H.Ranjitha <rajithah08@gmail.com>
2022-11-08 12:28:37 +0530

    else-if statement is done

---
+++ ControlStatement/src/Language.java
@@ -0,0 +1,19 @@
+
+public class Language {
+
+	public static void main(String[] args) {
+		String lang = "java";
+		if(lang == "Python") {
+			System.out.println("Language is python");
+		}else if(lang == "C++") {
+			System.out.println("lang is c++");
+		}else if(lang == "java") {
+			System.out.println("lang is java");
+		}else {
+			System.out.println(lang); }
+				
+			}
+
+	}
+
+

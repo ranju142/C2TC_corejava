8ead4ccac1cd1b524bb66810a532601c72bdd3a3
H.Ranjitha <rajithah08@gmail.com>
2022-11-08 12:38:12 +0530

    nested if done

---
+++ ControlStatement/src/Language1.java
@@ -0,0 +1,21 @@
+
+public class Language1 {
+
+	public static void main(String[] args) {
+		String lang ="java";
+		if(lang == "HLL & IL") {
+			if(lang == "python") {
+				System.out.println("Language1 is Python");
+			}else if(lang == "c++") {
+				System.out.println("Lang is c++");
+			}else if(lang == "java") {
+				System.out.println("Lang is java");
+			}else {
+				System.out.println(lang); }
+			
+			}
+		}
+
+	}
+
+

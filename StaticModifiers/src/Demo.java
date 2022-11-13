12966dcf07786289a58c30e57a596bc4bdfdcac1
H.Ranjitha <rajithah08@gmail.com>
2022-11-13 19:46:01 +0530

    java static block done

---
+++ StaticModifiers/src/Demo.java
@@ -0,0 +1,14 @@
+ class Demo {
+	 static
+	 {
+		 System.out.println("static block is involved");
+	 }
+    public static void main(String[] args) {
+    	{
+    		System.out.println("Hello main");
+    		
+    	}
+
+	}
+
+}

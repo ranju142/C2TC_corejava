223d2473c871ecf96b36f8dba389143ed708bb3b
H.Ranjitha <rajithah08@gmail.com>
2022-11-14 15:27:49 +0530

    first commit

---
+++ ThisKeyword/src/hello.java
@@ -0,0 +1,21 @@
+class hello 
+{
+	hello()
+	{
+		this(5);
+		System.out.println("hello a");
+	}
+	hello(int x)
+	{
+		System.out.println(x);
+	}
+}
+class main
+{
+   public static void main(String args[]) {
+	   {
+            hello a=new hello();
+	}
+
+}
+}

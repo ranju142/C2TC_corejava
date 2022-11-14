2d269f5e51c108114f05fe4d21fb43c24789b62e
H.Ranjitha <rajithah08@gmail.com>
2022-11-14 15:20:58 +0530

    first commit

---
+++ ThisKeyword/src/demo1.java
@@ -0,0 +1,21 @@
+class demo1 {
+  demo1()
+  {
+	  System.out.println("hello a");
+  }
+  demo1(int x)
+  {
+	  this();
+	  System.out.println(x);
+  }
+  }
+class TestThis5
+{
+	public static void main(String[] args) 
+	{
+		demo a=new demo(10);
+		
+
+	}
+}
+}

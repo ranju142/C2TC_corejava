3747607b7a181401e2582cd5265d2832801d1386
H.Ranjitha <rajithah08@gmail.com>
2022-11-15 23:02:02 +0530

    first commit

--- Inheritence/src/Animal.java
+++ Inheritence/src/Animal.java
@@ -1,8 +1,21 @@
-
-public class Animal {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Animal {
+    void eat()
+    {
+    	System.out.println("eating...");
+    }
+    }
+class dog extends Animal {
+	void bark()
+	{
+		System.out.println("barking...");
+	}
+}
+class demo
+{
+         public static void main(String[] args) {
+		 dog d=new dog();
+		 d.bark();
+		 d.eat();
 
 	}
 

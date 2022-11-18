a78a9dd85067b3a524a44c8c1826c66e3ece2ce6
H.Ranjitha <rajithah08@gmail.com>
2022-11-18 20:43:43 +0530

    first commit

--- Inheritence/src/Demo2.java
+++ Inheritence/src/Demo2.java
@@ -1,8 +1,28 @@
-
-public class Demo2 {
+class Animal
+{
+	void eat()
+	{
+		System.out.println("eating....");
+	}
+}
+class Dog extends Animal
+{
+	void eat() {
+		System.out.println("eating bread...");
+	}
+	void bark() {
+		System.out.println("barking...");
+	}
+	void work() {
+		super.eat();
+		bark();
+	}
+}
+class Demo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Dog d=new Dog();
+		d.work();
 
 	}
 

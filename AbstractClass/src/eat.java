48cef448e2b662fd5542923b9355ab577159cacd
H.Ranjitha <rajithah08@gmail.com>
2022-11-20 08:41:07 +0530

    first commit

---
+++ AbstractClass/src/eat.java
@@ -0,0 +1,19 @@
+abstract class Animal
+{
+ abstract void eat1();
+}
+class Herbivores extends Animal
+{
+void eat1()
+{
+System.out.print("I am a vegetarian");
+}
+}
+class Carnivores extends Animal {
+void eat()
+{
+System.out.print("I am non-vegetarian");
+}
+public static void main(String args[])
+{
+Animal goat = new Animal();} }
